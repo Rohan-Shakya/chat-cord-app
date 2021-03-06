203343f6f5a9ba7c6483d39f28445c3a63f35f96
Rohan <rohanshakya254@gmail.com>
2021-03-06 20:58:29 +0545

    react state managed with useContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AuthState from './context/AuthState';
+import setAuthToken from './utils/setAuthToken';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>React</h1>
-    </div>
-  );
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
+
+const App = () => {
+  return (
+    <AuthState>
+      <Router>
+        <h1>React</h1>
+      </Router>
+    </AuthState>
+  );
+};
 
 export default App;
